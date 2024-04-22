717f6dd9fa704bfb5c68097635104432c9a6815d
noahgiboney <noahgiboney@gmail.com>
2024-04-21 18:15:18 -0700

    fix: catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -9,9 +9,9 @@ import re
 router = APIRouter()
 @router.get("/catalog/", tags=["catalog"])
 def get_catalog():
-    # Fetch potions for sale from the potions table
+    # fetch potions from the potions table
     sql = """
-    SELECT id, name, sku, price, quantity, green, red, blue, dark
+    SELECT id, name, sku, price, quantity, red, green, blue, dark
     FROM potions
     WHERE quantity > 0;
     """
@@ -22,11 +22,8 @@ def get_catalog():
         result = connection.execute(sqlalchemy.text(sql))
         potion_data = result.fetchall()
 
-        if not potion_data:
-            raise HTTPException(status_code=404, detail="No potions available for sale.")
-
         for potion in potion_data:
-            potion_type = [potion.green, potion.red, potion.blue, potion.dark]
+            potion_type = [potion.red, potion.green ,potion.blue, potion.dark]
 
             try:          
                 potions_for_sale.append({
